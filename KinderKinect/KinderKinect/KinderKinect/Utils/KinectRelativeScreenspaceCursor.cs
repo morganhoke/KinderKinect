4ed4a479d34b5773efe769bff003dc6d203a23f3
Morgan <morgan.hoke@gmail.com>
2013-04-04 14:48:09 -0500

    Added stub for screenspace cursor and a plane model

---
+++ KinderKinect/KinderKinect/KinderKinect/Utils/KinectRelativeScreenspaceCursor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KinderKinect.Utils
+{
+    /// <summary>
+    /// Acts as a cursor when we are intersted in the player's hands at a position in the world
+    /// </summary>
+    class KinectRelativeScreenspaceCursor //: ICursor, IKinectListener
+    {
+    }
+}
